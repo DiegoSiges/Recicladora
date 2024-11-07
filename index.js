cc0c924d586cfd974f7a5e91074b262a44bd1313
Diego Mazzola <diego.mazzola@sistemasiges.com.ar>
2024-11-07 11:24:07 -0300

    Simplifying sport.on under the assumption that Arduino will not report a value multiple times for a single event, such as '444', but a single one

--- index.js
+++ index.js
@@ -29,16 +29,15 @@ sport.on('data', function(data){
 }); */
 
 
-/* The routine for sport.on below is written now to be used with the USB port of an Arduino clone. 
-In case the Arduino sends the same value multiple times, there is an if statement b
-elow to take only one of those values. 
+/* The routine for sport.on below is handles the input from Arduino, deriving the value to index.html 
+and to siges_ArduinoHandler.py. 
 
 Arduino can send the following values: 1: pag QR. 2: pag llenado. 4:incr cuenta botellas. 6: pag insertar 
 contenedor. 0: pag inicio con contador reiniciado. The routine uses only 0, 1 and 4: 0 to reinitiate 
 bottle counter; 4 to increment counter; 1 to send accumulation intention.
 The Siges Python script will have to return a value indicating success or failure sending the 
 accumulation request. In case the accumulation request fails, instead of the QR page the system will have 
-to show a pag  indicating the bottles total and a message to contact personnel on site to do the transaction
+to show a page indicating the bottles total and a message to contact personnel on site to do the transaction
 manually. 
 		
 Variable arduinoData holds the value reported by Arduino, which is passed to python
@@ -46,36 +45,25 @@ Variable arduinoData holds the value reported by Arduino, which is passed to pyt
 
 sport.on('data', function(data){
   
-  inputData=data.toString();
+  arduinoData=data.toString();
   
-  let arduinoData="7";
-
-  if (inputData.search("1")>=0){
-    arduinoData="1";
-  }  
-  else if (inputData.search("4")>=0){
-    arduinoData="4";
-  }
-  else if (inputData.search("0")>=0){ 
-    arduinoData="0";
-  }
   console.log("ArduinoData: ");
   console.log(arduinoData);
 
-  io.emit('arduino:data',{data:inputData}); // Sending data to index.html
+  io.emit('arduino:data',{data:arduinoData}); // Sending data to index.html
 
 // Spawing the python script to send the accumulation intention request
-// if Arduino data is valid
+// if Arduino data is applicable
   if (arduinoData=="0" || arduinoData=="1" || arduinoData=="4"){
-  const { spawn } = require ('child_process');
-  const dummyOutput = [];
+    const { spawn } = require ('child_process');
+    const dummyOutput = [];
 
-  const pyCatcher = spawn('python', ['siges_ArduinoInputHandler.py', arduinoData]);
-  pyCatcher.stdout.on('data', function(data) {
-    dummyOutput.push(parseFloat(data));
-    console.log(dummyOutput);
-  });}
-
+    const pyCatcher = spawn('python', ['siges_ArduinoInputHandler.py', arduinoData]);
+    pyCatcher.stdout.on('data', function(data) {
+      dummyOutput.push(parseFloat(data));
+      console.log(dummyOutput);
+    });
+  }
 });
 
 
