a4ba663e88c277439a7a99d4e3a91952cac436b7
Diego Mazzola <diego.mazzola@sistemasiges.com.ar>
2024-11-12 12:31:37 -0300

    Holding connection failure state for at least 3 minutes before any other Arduino input is accepted, for the user to request assistance

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const SerialPort = require('serialport');
 
+let connAttemptFailTime; // Variable to register when a connection attempt failed
+let connAttemptFailFlag = 0; // Flag to indicate whether a connection failure happened
+
+
 const server = app.listen(3000, function () {
   console.log('Listo el puerto 3000!');
 });
@@ -35,10 +39,12 @@ and to siges_ArduinoHandler.py.
 Arduino can send the following values: 1: pag QR. 2: pag llenado. 4:incr cuenta botellas. 6: pag insertar 
 contenedor. 0: pag inicio con contador reiniciado. The siges_ArduinoHandler acts only on 0, 1 and 4: 0 to 
 reinitiate bottle counter; 4 to increment counter; 1 to send accumulation intention.
-The Siges Python script will have to return a value indicating success or failure sending the 
-accumulation request. In case the accumulation request fails, instead of the QR page the system will have 
-to show a page indicating the bottles total and a message to contact personnel on site to do the transaction
-manually. 
+The Siges Python script returns a value indicating whether the accumuation request failed or succeded (success
+is a value in the 200s).In case the accumulation request fails, instead of the QR page the system currently
+shows the bottle counter page with an error message on top. Then the system holds this state for 3 minutes. 
+
+This is a temporary solution. The final solutionis to show a new slide with an image indicating the bottles 
+total and a message to contact personnel on site to do the transaction manually. 
 		
 Variable arduinoData holds the value reported by Arduino, which is passed to python
 */
@@ -51,26 +57,43 @@ sport.on('data', function(data){
   console.log(arduinoData);
 
 // Spawing the python script to send the accumulation intention request
-// if Arduino data is applicable 
-  if (arduinoData=="0" || arduinoData=="1" || arduinoData=="4"){
-    const { spawn } = require ('child_process');
-    const pyOutput = [];
+// if Arduino data is applicable. First, must check if 3 minutes elapsed
+// since a connection attempt failure. 
 
-    const pyCatcher = spawn('python', ['siges_ArduinoInputHandler.py', arduinoData]);
-    pyCatcher.stdout.on('data', function(data) {
-      pyOutput.push(parseFloat(data));
-      console.log(pyOutput);
-      if (pyOutput<"200" || pyOutput>"299"){
-        console.log("Bad code");
-        io.emit('arduino:data', {data:"9"});  // Sending error code to index.html
-      }
-      else {
+  console.log("conn times ");
+  console.log(connAttemptFailTime);
+  console.log(`Curr time ${Date.now()}`);
+
+  if (connAttemptFailFlag==0) {
+
+    if (arduinoData=="0" || arduinoData=="1" || arduinoData=="4"){
+      const { spawn } = require ('child_process');
+      const pyOutput = [];
+
+      const pyCatcher = spawn('python', ['siges_ArduinoInputHandler.py', arduinoData]);
+      pyCatcher.stdout.on('data', function(data) {
+        pyOutput.push(parseFloat(data));
+        console.log(pyOutput);
+        if (pyOutput<"200" || pyOutput>"299"){
+          console.log("Bad code");
+        
+          connAttemptFailFlag = 1;
+          connAttemptFailTime = Date.now();
+
+          // console.log(connAttemptFailTime);
+          io.emit('arduino:data', {data:"9"});  // Sending error code to index.html
+        }
+        else {
         io.emit('arduino:data',{data:arduinoData}); // Sending data to index.html
-      }
-    });
+        }
+      });
+    }
+    else {
+      io.emit('arduino:data',{data:arduinoData}); // Sending data to index.html
+    }
   }
-  else {
-    io.emit('arduino:data',{data:arduinoData}); // Sending data to index.html
+  else if (Date.now() - connAttemptFailTime > 180000){
+    connAttemptFailFlag = 0; // resets the flag if more than 3 minutes elapsed
   }
 });
 
